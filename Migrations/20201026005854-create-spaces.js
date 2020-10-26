3e97224e6256dedfb9e636a3543cc5bca8a80453
lucianaMarat <luciana.maratabyla@ynov.com>
2020-10-26 08:00:45 +0100

    model space migration

---
+++ Migrations/20201026005854-create-spaces.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('spaces', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      numberSpace: {
+        type: Sequelize.INTEGER
+      },
+      level: {
+        type: Sequelize.INTEGER
+      },
+      occupied: {
+        type: Sequelize.BOOLEAN
+      },
+      timeOccupied: {
+        type: Sequelize.STRING
+      },
+      userId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('spaces');
+  }
+};
